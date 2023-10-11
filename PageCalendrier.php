e88e9cc59729baf8d3c1a5dddd07a724d379d444
louis <leleul@3il.fr>
2023-10-11 09:57:44 +0200

    ajout calendrier

---
+++ PageCalendrier.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+  <meta charset='utf-8' />
+  <link rel="stylesheet" href="css/main.css">
+  <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
+  <script>
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var calendarEl = document.getElementById('calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+          left: '',
+          center: 'title',
+          right: 'prev,next today'
+        },
+        buttonText: {
+          today: 'Aujourd\'hui'
+        },
+        locale: 'fr',
+        firstDay: 1
+      });
+      calendar.render();
+    });
+
+  </script>
+</head>
+<body>
+<?php
+include_once ("./HTML/header.html");
+?>
+<div id='calendar'></div>
+<?php
+include_once ("./HTML/footer.html");
+?>
+</body>
+</html>
