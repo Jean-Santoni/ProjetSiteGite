bda949b55cf4a9cc71a65629189d0cafd1b91a37
louis <leleul@3il.fr>
2023-10-11 10:57:37 +0200

    Ajout de la connection a la base de donnee avec mdp crypte

--- Login.php
+++ Login.php
@@ -4,13 +4,23 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Récupérer les données du formulaire
   $username = $_POST['username'];
-  $password = $_POST['password'];
+  $password = md5($_POST['password']);
 
   // Vérifier les informations d'authentification dans la base de données
   // (Vous devez vous connecter à votre base de données ici)
+  $conn = new PDO("mysql:host=localhost;dbname=gitefiguies", 'root', '');
+  $req = "SELECT * FROM utilisateur WHERE NomUtilisateur = :pLogin AND MotDePasse = :pPassword;";
+
+  $stmt = $conn->prepare($req);
+  $stmt->bindParam(':pLogin', $username, PDO::PARAM_STR);
+  $stmt->bindParam(':pPassword', $password, PDO::PARAM_STR);
+  $stmt->execute();
+
+  $result = array();
+  $result = $stmt->fetchAll();
 
   // Si l'authentification réussit
-  if (true) {
+  if ($result != null) {
     // Stocker l'identifiant de l'utilisateur dans une session
     $_SESSION['username'] = $username;
 
@@ -19,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
   } else {
     $error_message = "Nom d'utilisateur ou mot de passe incorrect";
+    echo "C'est dla meeeeerrde";
   }
 }
 ?>
